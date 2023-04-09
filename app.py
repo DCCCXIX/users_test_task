68dcffe37ba6d61f5cb8540c493de3136ec975a0
DCCCXIX <igor.demidion@gmail.com>
2023-04-09 17:39:22 +0300

    proper docstrings and type hints

--- app.py
+++ app.py
@@ -4,18 +4,38 @@ import csv
 
 app = Flask(__name__)
 
-def read_csv_file():
-    """Helper function to read csv files"""
+def read_csv_file() -> list:
+    """
+    Reads a CSV file containing user data.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary represents a user's data
+            with keys as column names and values as corresponding values from the CSV file.
+    Raises:
+        FileNotFoundError: If the CSV file is not found, a new empty table is created by calling write_csv_file() function,
+            and then the file is read again.
+    """
     try:
-        with open('users.csv', mode='r') as file:
+        with open('users.csv', mode='r') as file:            
             reader = csv.DictReader(file)
-            return list(reader)
     except FileNotFoundError:
         # if table is not found - create an empty table
         write_csv_file(None)
+        reader = csv.DictReader(file)
+    return list(reader)
 
-def write_csv_file(users):
-    """Helper function to write csv files"""
+def write_csv_file(users: list) -> None:
+    """
+    Writes user data to a CSV file.
+
+    Args:
+        users (list): A list of dictionaries, where each dictionary represents a user's data
+            with keys as column names and values as corresponding values.
+
+    Note:
+        If the users list is empty, the function will create an empty table with only the header row
+        to represent an empty CSV file.
+    """
     fieldnames = ['id', 'name', 'age', 'city']
     with open('users.csv', mode='w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -24,13 +44,34 @@ def write_csv_file(users):
             # so we don't write rows while creating an empty table
             writer.writerows(users)
 
-def generate_user_id(users):
-    """Helper function to generate user IDs"""
+def generate_user_id(users: list) -> int:
+    """
+    Generates a unique user ID based on the existing list of users.
+
+    Args:
+        users (list): A list of dictionaries, where each dictionary represents a user's data
+            with keys as column names and values as corresponding values.
+
+    Returns:
+        int: A unique user ID that is one greater than the maximum user ID found in the existing users list.
+            If the users list is empty, the function returns 1 as the initial user ID.
+    """
     ids = [int(user['id']) for user in users]
     return max(ids) + 1 if ids else 1
 
-def validate_user_data(user):
-    """User data validation"""
+def validate_user_data(user: dict) -> bool:
+    """
+    Validates user data to ensure it meets the expected format and constraints.
+
+    Args:
+        user (dict): A dictionary representing user data with keys as column names and values as corresponding values.
+
+    Returns:
+        bool: True if the user data is valid, False otherwise.
+
+    Raises:
+        AssertionError: If any of the validation checks fail, an AssertionError is raised with a description of the failed check.
+    """
     try:
         if 'id' in user: assert isinstance(user.get('id'), int) and user.get('id') > 0 # ids start with 1
         if 'name' in user: assert isinstance(user.get('name'), str) and len(user.get('name')) < 50
@@ -41,8 +82,14 @@ def validate_user_data(user):
     return True
 
 @app.route('/users', methods=['GET'])
-def get_users():
-    """Returns user list by name or city"""
+def get_users() -> dict:
+    """
+    Retrieves a list of users from the CSV file, filtered by name or city.
+
+    Returns:
+        dict: A dictionary containing the list of filtered users, if name or city query parameters are provided,
+            otherwise returns the complete list of users from the CSV file.
+    """
     users = read_csv_file()
     name = request.args.get('name')
     city = request.args.get('city')
@@ -56,8 +103,14 @@ def get_users():
         return {'users': users}, 200
 
 @app.route('/users', methods=['POST'])
-def add_user():
-    """Adds a user to the existing table"""    
+def add_user() -> dict:
+    """
+    Adds a user to the existing CSV table.
+
+    Returns:
+        dict: A dictionary containing the added user data if the user data is valid and successfully added to the table.
+            Otherwise, returns an error dictionary with a 400 status code indicating invalid user data.
+    """
     user = request.get_json()    
     if not validate_user_data(user):
         return {'error': 'Invalid user data'}, 400
@@ -68,8 +121,17 @@ def add_user():
     return {'user': user}, 201
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    """Deletes a user from the existing table by id"""
+def delete_user(user_id: int) -> dict:
+    """
+    Deletes a user from the existing CSV table by user ID.
+
+    Args:
+        user_id (int): The ID of the user to be deleted.
+
+    Returns:
+        dict: A dictionary containing the deleted user data if the user is found and successfully deleted from the table.
+            Otherwise, returns an error dictionary with a 404 status code indicating user not found.
+    """
     users = read_csv_file()
     if not validate_user_data(user):
         return {'error': 'Invalid user data'}, 400    
@@ -87,8 +149,17 @@ def delete_user(user_id):
         return {'error': 'User not found'}, 404
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
-def change_user(user_id):
-    """Change user info"""
+def change_user(user_id: int) -> dict:
+    """
+    Changes user information in the existing CSV table by user ID.
+
+    Args:
+        user_id (int): The ID of the user to be updated.
+
+    Returns:
+        dict: A dictionary containing the updated user data if the user is found and successfully updated in the table.
+            Otherwise, returns an error dictionary with a 404 status code indicating user not found.
+    """
     user = request.get_json()
     if not validate_user_data(user):
         return {'error': 'Invalid user data'}, 400
